cbee28a208d75b23a2bce0e0566780be2c615185
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-14 15:03:21 +0500

    created verify email page

--- resources/js/pages/auth/reset-password.tsx
+++ resources/js/pages/auth/reset-password.tsx
@@ -36,7 +36,7 @@ export default function ResetPassword({ token, email }: ResetPasswordProps) {
     };
 
     return (
-        <AuthLayout title="Reset password" description="Please enter your new password below" bgImage='/images/dashboard-bg.png'>
+        <AuthLayout title="Reset password" description="Please enter your new password below" bgImage=''>
             <Head title="Reset password" />
 
             <form method="POST" onSubmit={submit}>
