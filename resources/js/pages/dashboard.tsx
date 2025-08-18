b90c670303a9a9f839db06729cb81188188e563d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-18 13:38:03 +0500

    created cards

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { CardProgress } from '@/components/card-progress';
 import { Card, CardContent } from '@/components/ui/card';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import { Progress } from '@/components/ui/progress';
@@ -5,6 +6,7 @@ import AppLayout from '@/layouts/app-layout';
 import { type DashboardCard, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { User as UserIcon } from 'lucide-react';
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,31 +21,31 @@ const dashboardCardData: DashboardCard[] = [
         progress: 74,
         count: 12,
         icon: UserIcon,
-        description: 'All employees are ',
+        description: 'Current employees',
         color: '#29AB91'
     },
     {
-        title: 'Total Employees',
+        title: 'Total Late',
         progress: 74,
-        count: 12,
+        count: 5,
         icon: UserIcon,
-        description: 'All employees are ',
+        description: 'Late employees ',
         color: '#FFA600'
     },
     {
-        title: 'Total Employees',
+        title: 'Today’s Absentees',
         progress: 74,
-        count: 12,
+        count: 3,
         icon: UserIcon,
-        description: 'All employees are ',
+        description: 'Absent employees',
         color: '#FF5630'
     },
     {
-        title: 'Total Employees',
+        title: 'Pending Leave Request',
         progress: 74,
-        count: 12,
+        count: 4,
         icon: UserIcon,
-        description: 'All employees are ',
+        description: 'Employee leaves',
         color: '#377DFF'
     },
 ]
@@ -58,17 +60,17 @@ export default function Dashboard() {
                         <div key={index} className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                             {/* <div className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" /> */}
                             <Card className={`bg-[${item.color}] text-accent size-full dark:bg-accent dark:text-primary`}>
-                                <CardContent className='flex gap-2'>
+                                <CardContent className='flex gap-2 justify-between'>
                                     <div className='flex flex-col gap-3'>
                                         <h3 className='text-sm'>{item.title}</h3>
-                                        <h2 className='text-2xl font-semibold'>{item.count}</h2>
+                                        <h2 className='text-2xl font-semibold'>{item.count < 10 ? `0${item.count}` : item.count}</h2>
                                         <div className='flex items-center gap-2'>
-                                            {item.icon && <item.icon className="w-8 h-8 text-white mb-2" />}
-                                            <p>{item.description}</p>
+                                            {item.icon && <item.icon className="w-6 h-6 text-white my-2" />}
+                                            <p className='text-sm'>{item.description}</p>
                                         </div>
                                     </div>
-                                    <div className=''>
-                                        <Progress />
+                                    <div className='items-center flex'>
+                                        <CardProgress />
                                     </div>
                                 </CardContent>
                             </Card>
